f1a964ecec867bf4e0576cc5e927357705187488
Dan <dan@danthekarman.tk>
2013-08-30 14:15:38 -0700

    ChatColor + PacketID

---
+++ src/faucitt/ChatColor.java
@@ -0,0 +1,29 @@
+package faucitt;
+
+public class ChatColor {
+	public static final String sym = "\u00A7";
+
+	public static final String Black = sym + "0";
+	public static final String DarkBlue = sym + "1";
+	public static final String DarkGreen = sym + "2";
+	public static final String DarkAqua = sym + "3";
+	public static final String DarkRed = sym + "4";
+	public static final String Purple = sym + "5";
+	public static final String Orange = sym + "6";
+	public static final String Gray = sym + "7";
+	public static final String DarkGray = sym + "8";
+	public static final String Blue = sym + "9";
+	public static final String Lime = sym + "a";
+	public static final String Aqua = sym + "b";
+	public static final String Red = sym + "c";
+	public static final String Pink = sym + "d";
+	public static final String Yellow = sym + "e";
+	public static final String White = sym + "f";
+
+	public static final String Gibberish = sym + "k";
+	public static final String Bold = sym + "l";
+	public static final String Strike = sym + "m";
+	public static final String Underline = sym + "n";
+	public static final String Italic = sym + "o";
+	public static final String Plain = sym + "r";
+}
